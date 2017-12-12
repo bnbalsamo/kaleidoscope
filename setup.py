8e996d8ee1c1d83dd2e32232091194ac868fd91f
brian <balsamo@uchicago.edu>
2017-12-12 11:57:31 -0600

    Bump version: 0.0.2 → 0.0.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 setup(
     name="kaleidoscope",
     description="An IIIF image server",
-    version="0.0.2",
+    version="0.0.3",
     long_description=readme(),
     author="Brian Balsamo",
     author_email="brian@brianbalsamo.com",
